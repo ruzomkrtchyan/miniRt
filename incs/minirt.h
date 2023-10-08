a43047368b0a8b0794790a508c765283c7c7984e
Ruzanna Mkrtchyan <rmkrtchy@c2r3s4.42yerevan.am>
2023-10-08 12:40:09 +0400

    Start

---
+++ incs/minirt.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmkrtchy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 18:51:35 by rmkrtchy          #+#    #+#             */
+/*   Updated: 2023/10/05 15:01:29 by rmkrtchy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <math.h>
+
+typedef struct s_cam
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_cam;
+
+
+#endif
