b2d9de7a3d992151855e7b89dc97b8500452b7e2
Ruzanna Mkrtchyan <rmkrtchy@c2r3s4.42yerevan.am>
2023-10-30 20:09:03 +0400

    created circle

--- src/mlx_start/run_mlx.c
+++ src/mlx_start/run_mlx.c
@@ -6,7 +6,7 @@
 /*   By: rmkrtchy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:26:38 by vhovhann          #+#    #+#             */
-/*   Updated: 2023/10/30 19:38:13 by rmkrtchy         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:07:32 by rmkrtchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,17 +63,17 @@ void	mlx_create(t_scene *scene)
 	scene->mlx = (t_mlx *)malloc(sizeof(t_mlx));
 	scene->mlx->mlx = mlx_init();
 	scene->mlx->mlx_win = mlx_new_window(scene->mlx->mlx, 1080, 720, "MiniRt");
-	scene->mlx->img = mlx_new_image(scene->mlx->mlx, 1920, 1080);
-	scene->mlx->addr = mlx_get_data_addr(scene->mlx->img, \
+	/*// scene->mlx->img = mlx_new_image(scene->mlx->mlx, 1920, 1080);
+	// scene->mlx->addr = mlx_get_data_addr(scene->mlx->img, \
 								&scene->mlx->bits_per_pixel, \
 								&scene->mlx->line_length, &scene->mlx->endian);
 	// my_mlx_pixel_put(scene->mlx, 5, 5, 0x00FF0000);
+	// mlx_put_image_to_window(scene->mlx->mlx, scene->mlx->mlx_win, \
+								scene->mlx->img, 0, 0); */
 	ray_tracing(scene);
-	mlx_put_image_to_window(scene->mlx->mlx, scene->mlx->mlx_win, \
-								scene->mlx->img, 0, 0);
 	mlx_hook(scene->mlx->mlx_win, 2, 0, &mlx_keypress, scene);
-	mlx_hook(scene->mlx->mlx_win, 4, 0, &button_press, scene);
-	mlx_hook(scene->mlx->mlx_win, 6, 0, &mouse, scene);
+	// mlx_hook(scene->mlx->mlx_win, 4, 0, &button_press, scene);
+	// mlx_hook(scene->mlx->mlx_win, 6, 0, &mouse, scene);
 	mlx_hook(scene->mlx->mlx_win, 17, 0, &close_window, scene);
 	mlx_loop(scene->mlx->mlx);
 }
--- src/ray_tracing/raytracing.c
+++ src/ray_tracing/raytracing.c
@@ -6,7 +6,7 @@
 /*   By: rmkrtchy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:53:31 by rmkrtchy          #+#    #+#             */
-/*   Updated: 2023/10/30 19:40:12 by rmkrtchy         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:07:02 by rmkrtchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ int	sphere_intersection(t_cam *cam, t_vect *ray, t_sph *sph)
 
 	x1 = 0;
 	x2 = 0;
-	cam_to_sphere = substraction_vect(cam->dir, sph->coord);
+	cam_to_sphere = substraction_vect(cam->pos, sph->coord);
 	b = 2 * (dot_product_vect(cam_to_sphere, ray));
-	c = dot_product_vect(cam_to_sphere,cam_to_sphere) * sph->radius;
-	disc = b * b - (4 * c);
+	c = dot_product_vect(cam_to_sphere,cam_to_sphere) - (sph->radius * sph->radius);
+	disc = (b * b) - (4 * c);
 	free(cam_to_sphere);
 	if (disc < 0)
 		return (0);
-	x1 = ((b * -1) - sqrt(disc)) / 2;
-	x2 = ((b * -1) + sqrt(disc)) / 2;
+	x1 = ((b * (-1)) - sqrt(disc)) / 2;
+	x2 = ((b * (-1)) + sqrt(disc)) / 2;
 	if (x1 > 0)
 		return (1);
 	return (0);
@@ -49,23 +49,24 @@ void	ray_tracing(t_scene *scene)
 	t_vplane	*v_plane;
 	t_vect		*ray;
 
-	y_angle = (scene->height / 2) * (-1);
+	y_angle = (scene->height / 2);
 	v_plane = get_vplane(scene->width, scene->height);
 	mlx_y = 0;
-	while (y_angle >= scene->height / 2)
+	while (y_angle >= scene->height / 2 * (-1))
 	{
 		mlx_x = 0;
 		y_ray = y_angle * v_plane->y_pixel;
 		x_angle = ((scene->width / 2) * (-1));
-		while (x_angle >= scene->width / 2)
+		while (x_angle <= scene->width / 2)
 		{
 			x_ray = x_angle * v_plane->x_pixel;
 			ray = new_vect(x_ray, y_ray, -1);
+			norm_vect(ray);
 			if (sphere_intersection(scene->cam, ray, scene->sph))
 				color = 16777215;
 			else
 				color = 0;
-			my_mlx_pixel_put(scene->mlx, mlx_x, mlx_y, color);
+			mlx_pixel_put(scene->mlx, scene->mlx->mlx_win, mlx_x, mlx_y, color);
 			free(ray);
 			x_angle++;
 			mlx_x++;
